685a5a31cbddd23537c6d074e843ac308a6f9731
omrakibe <rakibeom30@gmail.com>
2025-04-07 23:33:48 +0530

    Removed pricing tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            
           </Nav>
         </Container>
       </Navbar>
